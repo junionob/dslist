d1864c5c41d4730f9130d9962c9281c330d1b552
junionob <junio.waldomiro@gmail.com>
2025-01-22 19:27:04 -0300

    Homolog ok

--- dslist/src/main/java/com/devsuperior/dslist/projections/GameMinProjection.java
+++ dslist/src/main/java/com/devsuperior/dslist/projections/GameMinProjection.java
@@ -5,7 +5,7 @@ import java.util.Date;
 public interface GameMinProjection {
     Long getId();
     String getTitle();
-    Integer getYear();
+    Integer getGameYear();
     String getImgUrl();
     String getShortDescription();
     Integer getPosition();
