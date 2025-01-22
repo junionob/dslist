19a8d250004f2500ab89518ec50a0637575fed0e
junionob <junio.waldomiro@gmail.com>
2025-01-22 09:38:27 -0300

    Get lists

---
+++ dslist/src/main/java/com/devsuperior/dslist/servicies/GameListService.java
@@ -0,0 +1,27 @@
+package com.devsuperior.dslist.servicies;
+
+import com.devsuperior.dslist.dto.GameDTO;
+import com.devsuperior.dslist.dto.GameListDTO;
+import com.devsuperior.dslist.dto.GameMinDTO;
+import com.devsuperior.dslist.entities.Game;
+import com.devsuperior.dslist.repositories.GameListRepository;
+import com.devsuperior.dslist.repositories.GameRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class GameListService {
+
+    @Autowired
+    public GameListRepository gameListRepository;
+
+
+    @Transactional(readOnly = true)
+    public List<GameListDTO> findAll() {
+        var result = gameListRepository.findAll().stream().map(GameListDTO::new).toList();
+        return result;
+    }
+}
