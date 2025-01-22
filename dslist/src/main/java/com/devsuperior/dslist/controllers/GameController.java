e466246f8e2317211cc38b0a8a23cd27a9ef1968
junionob <junio.waldomiro@gmail.com>
2025-01-21 23:17:30 -0300

    Get by id

--- dslist/src/main/java/com/devsuperior/dslist/controllers/GameController.java
+++ dslist/src/main/java/com/devsuperior/dslist/controllers/GameController.java
@@ -1,10 +1,12 @@
 package com.devsuperior.dslist.controllers;
 
+import com.devsuperior.dslist.dto.GameDTO;
 import com.devsuperior.dslist.dto.GameMinDTO;
 import com.devsuperior.dslist.entities.Game;
 import com.devsuperior.dslist.servicies.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,6 +18,11 @@ public class GameController {
 
     @Autowired
     private GameService service;
+
+    @GetMapping(value= "/{id}")
+    public GameDTO findById(@PathVariable Long id) {
+        return service.findById(id);
+    }
 
     @GetMapping
     public List<GameMinDTO> findAll() {
--- dslist/src/main/java/com/devsuperior/dslist/servicies/GameService.java
+++ dslist/src/main/java/com/devsuperior/dslist/servicies/GameService.java
@@ -1,8 +1,10 @@
 package com.devsuperior.dslist.servicies;
 
+import com.devsuperior.dslist.dto.GameDTO;
 import com.devsuperior.dslist.dto.GameMinDTO;
 import com.devsuperior.dslist.entities.Game;
 import com.devsuperior.dslist.repositories.GameRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,7 +15,12 @@ public class GameService {
 
     @Autowired
     public GameRepository gameRepository;
-    
+
+    public GameDTO findById(Long id) {
+        Game result = gameRepository.findById(id).get();
+        return new GameDTO(result);
+    }
+
     public List<GameMinDTO> findAll() {
         var result = gameRepository.findAll().stream().map(GameMinDTO::new).toList();
         return result;
